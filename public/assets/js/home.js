47f34a6431b9cbbf9e5d6d1f30524d8b5dbd2a5c
khuuwinnie <khuu.winnie@gmail.com>
2019-10-09 18:06:42 +0000

    Set up AJAX

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -3,6 +3,7 @@ const $setSignUp = $('#signup');
 const $submitButton = $('#submit');
 const $emailInput = $('#email');
 const $passwordInput = $('#password');
+const $message = $('#message');
 
 let authSetting = 'login';
 
@@ -20,13 +21,40 @@ function setAuth(setting) {
   }
 }
 
-function handleFormSubmit() {
+function handleFormSubmit(event) {
+  event.preventDefault();
+
   let email = $emailInput.val().trim();
   let password = $passwordInput.val().trim();
 
-  console.log(
-    `Email: ${email} Password: ${password} AuthSetting: ${authSetting}`
-  );
+  if (!email || !password) {
+    displayMessage ('Email and password fields cannot be blank.', 'danger');
+    return;
+  }
+
+  $emailInput.val('');
+  $passwordInput.val('');
+
+  authenticateUser(email, password);
+}
+
+function displayMessage(message, type) {
+  $message.text(message).attr('class', type);
+}
+
+function authenticateUser(email, password) {
+  $.ajax({
+    url: '/' + authSetting,
+    data: {
+      user: {
+        email,
+        password
+      }
+    },
+    method: 'POST'
+  }).then(function(data) {
+    console.log(data);
+  });
 }
 
 $setLogin.on('click', setAuth.bind(null, 'login'));
