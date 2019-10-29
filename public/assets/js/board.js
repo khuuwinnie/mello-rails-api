79d415e1bc9643eda47d95dd9c20457c023d88b9
Winnie Khuu <khuu.winnie@gmail.com>
2019-10-29 03:53:59 +0000

    Delete contributors function added

--- public/assets/js/board.js
+++ public/assets/js/board.js
@@ -34,7 +34,9 @@ function getBoard(id) {
     renderBoard();
   })
   .catch(function(err) {
-    location.replace('/boards');
+    if (err.statusText === 'Unauthorized') {
+      location.replace('/boards');
+    }
   });
 }
 
@@ -115,7 +117,16 @@ function renderBoard() {
 
 function renderContributors() {
   let $contributorListItems = board.users.map(function(user) {
-    let $contributorListItem = $('<li>').text(user.email);
+    let $contributorListItem = $('<li>');
+    let $contributorSpan = $('<span>').text(user.email);
+    let $contributorDeleteButton = $(
+        '<button class="danger">Remove</button>'
+    )
+        .data(user)
+        .on('click', handleContributorDelete);
+
+    $contributorListItem.append($contributorSpan, $contributorDeleteButton);
+
     return $contributorListItem;
   });
 
@@ -403,6 +414,22 @@ function openContributorModal() {
   displayMessage('');
 
   MicroModal.show('contribute');
+}
+
+function handleContributorDelete(event) {
+  let { id, email } = $(event.target).data();
+
+  $.ajax({
+    url: '/api/user_boards',
+    method: 'DELETE',
+    data: {
+      user_id: id,
+      board_id: board.id
+    }
+  }).then(function() {
+    init();
+    displayMessage(`Successfully removed user: ${email}`, 'success');
+  });
 }
 
 $contributorModalSaveButton.on('click', handleContributorSave);
